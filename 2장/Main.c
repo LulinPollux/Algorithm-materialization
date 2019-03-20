4f36f0392c8cfdc723ea23c6dec5ed72d349c456
Lulin Pollux <kistssy+dev@gmail.com>
2019-03-20 17:50:20 +0900

    함수 추가
    최대 숫자를 찾는 함수 추가함.

--- 2장/Main.c
+++ 2장/Main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 
 
 //최대공약수를 계산하는 함수
@@ -36,10 +36,25 @@ int greatestCommonFactor(int a, int b, int *result)
 	}
 }
 
+//최대 숫자를 찾는 함수
+int maxNumber(int data[10])
+{
+	int max = data[0];
+	for (int i = 1; i <= 9; i++)
+	{
+		if (data[i] > max)
+			max = data[i];
+	}
+	
+	return max;
+}
+
 int main()
 {
 	int retval;
 
+	//최대공약수를 계산하는 코드
+	//---------------------------------------------------------------
 	int input1, input2, result;
 	printf("2개의 정수 데이터 입력: ");
 	scanf_s("%d %d", &input1, &input2);
@@ -51,6 +66,16 @@ int main()
 		return 1;
 	}
 
-	printf("결과: %d", result);
+	printf("결과: %d\n", result);
+
+
+	//최대 숫자 찾는 코드
+	//---------------------------------------------------------------
+	printf("최대 숫자 찾기: 25 10 20 35 55 45 60 79 90 3 \n");
+	int data[10] = { 25, 10, 20, 35, 55, 45, 60, 79, 90, 3 };
+
+	retval = maxNumber(data);
+	printf("결과: %d\n", retval);
+
 	return 0;
 }
