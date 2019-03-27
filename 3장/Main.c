bdf1605967813ccdf5f0766e69c5f2966f1d7fc2
Lulin Pollux <kistssy+dev@gmail.com>
2019-03-27 17:59:54 +0900

    3장 코드 추가
    1. 난수생성 함수, 선택 정렬 함수를 추가함.

---
+++ 3장/Main.c
@@ -0,0 +1,97 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//난수를 생성하는 함수 (0 ~ range-1)
+int random(int *output, int count, int range)
+{
+	//오류제어: 범위보다 난수 개수가 더 많으면 중단함.
+	if (count > range)
+		return 1;
+
+	//불규칙 난수 생성 활성화
+	srand((unsigned int)time(NULL));
+
+	//난수를 생성한다.
+	for (int i = 0; i < count; i++)
+	{
+		output[i] = rand() % range;
+
+		//수가 중복되면 다시 뽑는다.
+		for (int j = 0; j < i; j++)
+		{
+			if (output[j] == output[i])
+			{
+				i--;
+				break;
+			}
+		}
+	}
+
+	return 0;
+}
+
+//선택 정렬을 수행하는 함수
+int selectSort(int *data, int count)
+{
+	int index = 0;
+	int min;
+
+	for (int i = 0; i < count; i++)
+	{
+		min = 9999;
+		for (int j = i; j < count; j++)
+		{
+			if (min > data[j])
+			{
+				min = data[j];
+				index = j;
+			}
+		}
+		int temp = data[i];
+		data[i] = data[index];
+		data[index] = temp;
+	}
+
+	return 0;
+}
+
+int main()
+{
+	int retval;
+	
+	//난수를 생성한다.
+	int data[100] = { 0 };
+	retval = random(data, sizeof(data) / sizeof(int), 100);
+	if (retval != 0)
+	{
+		printf("오류 발생: %d\n", retval);
+		return 1;
+	}
+
+	//데이터를 출력한다.
+	printf("데이터: ");
+	for (int i = 0; i < (sizeof(data) / sizeof(int)); i++)
+	{
+		printf("%d ", data[i]);
+	}
+	printf("\n\n");
+
+	//선택 정렬을 한다.
+	retval = selectSort(data, sizeof(data) / sizeof(int));
+	if (retval != 0)
+	{
+		printf("오류 발생: %d\n", retval);
+		return 1;
+	}
+
+	//결과를 출력한다.
+	printf("결과: ");
+	for (int i = 0; i < (sizeof(data) / sizeof(int)); i++)
+	{
+		printf("%d ", data[i]);
+	}
+	printf("\n\n");
+
+	return 0;
+}
